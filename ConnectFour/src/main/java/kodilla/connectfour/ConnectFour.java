c256c36dc9292e1dddad2091ef2d7b47cfd1cf3c
Mateusz Olszewski <matcrono@gmail.com>
2023-03-04 18:40:56 +0800

    seperate logic

--- ConnectFour/src/main/java/kodilla/connectfour/ConnectFour.java
+++ ConnectFour/src/main/java/kodilla/connectfour/ConnectFour.java
@@ -1,8 +1,6 @@
 package kodilla.connectfour;
 
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -11,23 +9,16 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ConnectFour extends Application {
 
-    static final int TILE_SIZE = 80;
-    static final int COLUMNS = 7;
+    static final int TILE_SIZE = 120;
+    static final int COLUMNS = 6;
     static final int ROWS = 7;
-
-    private boolean firstPlayerMove = true;
-    private Pane discRoot = new Pane();
-    private Disc[][] grid = new Disc[COLUMNS][ROWS];
+    GameLogic gameLogic = new GameLogic();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -44,7 +35,7 @@ public class ConnectFour extends Application {
     private Parent createGame() {
 
         Pane root = new Pane();
-        root.getChildren().add(discRoot);
+        root.getChildren().add(gameLogic.getDiscRoot());
 
         Shape gridShape = createGameBoard();
         root.getChildren().add(gridShape);
@@ -82,103 +73,11 @@ public class ConnectFour extends Application {
             final int column = x;
             rectangle.setOnMouseEntered(p -> rectangle.setFill(Color.rgb(150, 150, 0, 0.4)));
             rectangle.setOnMouseExited(p -> rectangle.setFill(Color.TRANSPARENT));
-            rectangle.setOnMouseClicked(p -> playDisc(new Disc(firstPlayerMove), column));
+            rectangle.setOnMouseClicked(p -> gameLogic.playDisc(new GameLogic.Disc(gameLogic.isFirstPlayerMove()), column));
 
             columnList.add(rectangle);
         }
         return columnList;
     }
 
-    // Game Logic
-    private static class Disc extends Circle {
-        private final boolean firstPlayer;
-
-        public Disc(boolean firstPlayer) {
-            super(TILE_SIZE / 2, firstPlayer ? Color.GREEN : Color.YELLOW);
-            this.firstPlayer = firstPlayer;
-
-            setCenterX(TILE_SIZE / 2);
-            setCenterY(TILE_SIZE / 2);
-        }
-    }
-
-    private void playDisc(Disc disc, int column) {
-        int row = ROWS - 1;
-        do {
-            if (!getDisc(column, row).isPresent())
-                break;
-            row--;
-        } while (row >= 0);
-        if (row < 0)
-            return;
-
-        grid[column][row] = disc;
-        discRoot.getChildren().add(disc);
-        disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 4);
-
-        final int currentRow = row;
-
-        TranslateTransition animation = new TranslateTransition(Duration.seconds(0.2), disc);
-        animation.setToY(row * (TILE_SIZE + 5) + TILE_SIZE / 4);
-        animation.setOnFinished(e -> {
-            if (gameEnded(column, currentRow)) {
-                gameOver();
-            }
-            firstPlayerMove = !firstPlayerMove;
-        });
-        animation.play();
-    }
-
-    private boolean gameEnded(int column, int row) {
-        List<Point2D> verticalVictory = IntStream.rangeClosed(row - 3, row + 3)
-                .mapToObj(r -> new Point2D(column, r))
-                .collect(Collectors.toList());
-
-        List<Point2D> horizontalVictory = IntStream.rangeClosed(column - 3, column + 3)
-                .mapToObj(c -> new Point2D(c, row))
-                .collect(Collectors.toList());
-
-        Point2D leftUp = new Point2D(column - 3, row - 3);
-        List<Point2D> leftAscendingVictory = IntStream.rangeClosed(0, 6)
-                .mapToObj(l -> leftUp.add(l, l))
-                .collect(Collectors.toList());
-
-        Point2D leftDown = new Point2D(column - 3, row + 3);
-        List<Point2D> leftdescendingVictory = IntStream.rangeClosed(0, 6)
-                .mapToObj(l -> leftDown.add(l, -l))
-                .collect(Collectors.toList());
-
-        return checkVictoryCondition(verticalVictory) || checkVictoryCondition(horizontalVictory)
-                || checkVictoryCondition(leftAscendingVictory) || checkVictoryCondition(leftdescendingVictory);
-    }
-
-    private boolean checkVictoryCondition(List<Point2D> points) {
-        int sequence = 0;
-        for (Point2D p : points) {
-            int column = (int) p.getX();
-            int row = (int) p.getY();
-
-            Disc disc = getDisc(column, row).orElse(new Disc(!firstPlayerMove));
-            if (disc.firstPlayer == firstPlayerMove) {
-                sequence++;
-                if (sequence >= 4) {
-                    return true;
-                }
-            } else {
-                sequence = 0;
-            }
-        }
-        return false;
-    }
-
-    private void gameOver() {
-        System.out.println("Game is Finished");
-        System.out.println((firstPlayerMove ? " Green" : "Yellow") + " Player Won");
-    }
-    private Optional<Disc> getDisc(int column, int row) {
-        if ((column < 0 || column >= COLUMNS)
-                || (row < 0 || row >= ROWS))
-            return Optional.empty();
-        return Optional.ofNullable(grid[column][row]);
-    }
 }
--- ConnectFour/src/main/java/kodilla/connectfour/GameLogic.java
+++ ConnectFour/src/main/java/kodilla/connectfour/GameLogic.java
@@ -1,2 +1,124 @@
-package kodilla.connectfour;public class GameLogic {
+package kodilla.connectfour;
+
+import javafx.animation.TranslateTransition;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static kodilla.connectfour.ConnectFour.*;
+
+public class GameLogic {
+
+    private boolean firstPlayerMove = true;
+    private Disc[][] grid = new Disc[COLUMNS][ROWS];
+
+    public Pane getDiscRoot() {
+        return discRoot;
+    }
+
+    private Pane discRoot = new Pane();
+
+    static class Disc extends Circle {
+        private final boolean firstPlayer;
+
+        public Disc(boolean firstPlayer) {
+            super(TILE_SIZE / 2, firstPlayer ? Color.GREEN : Color.YELLOW);
+            this.firstPlayer = firstPlayer;
+
+            setCenterX(TILE_SIZE / 2);
+            setCenterY(TILE_SIZE / 2);
+        }
+    }
+
+    void playDisc(Disc disc, int column) {
+        int row = ROWS - 1;
+        do {
+            if (!getDisc(column, row).isPresent())
+                break;
+            row--;
+        } while (row >= 0);
+        if (row < 0)
+            return;
+
+        grid[column][row] = disc;
+        discRoot.getChildren().add(disc);
+        disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 4);
+
+        final int currentRow = row;
+
+        TranslateTransition animation = new TranslateTransition(Duration.seconds(0.2), disc);
+        animation.setToY(row * (TILE_SIZE + 5) + TILE_SIZE / 4);
+        animation.setOnFinished(e -> {
+            if (gameEnded(column, currentRow)) {
+                gameOver();
+            }
+            firstPlayerMove = !firstPlayerMove;
+        });
+        animation.play();
+    }
+
+    private boolean gameEnded(int column, int row) {
+        List<Point2D> verticalVictory = IntStream.rangeClosed(row - 3, row + 3)
+                .mapToObj(r -> new Point2D(column, r))
+                .collect(Collectors.toList());
+
+        List<Point2D> horizontalVictory = IntStream.rangeClosed(column - 3, column + 3)
+                .mapToObj(c -> new Point2D(c, row))
+                .collect(Collectors.toList());
+
+        Point2D leftUp = new Point2D(column - 3, row - 3);
+        List<Point2D> leftAscendingVictory = IntStream.rangeClosed(0, 6)
+                .mapToObj(l -> leftUp.add(l, l))
+                .collect(Collectors.toList());
+
+        Point2D leftDown = new Point2D(column - 3, row + 3);
+        List<Point2D> leftdescendingVictory = IntStream.rangeClosed(0, 6)
+                .mapToObj(l -> leftDown.add(l, -l))
+                .collect(Collectors.toList());
+
+        return checkVictoryCondition(verticalVictory) || checkVictoryCondition(horizontalVictory)
+                || checkVictoryCondition(leftAscendingVictory) || checkVictoryCondition(leftdescendingVictory);
+    }
+
+    private boolean checkVictoryCondition(List<Point2D> points) {
+        int sequence = 0;
+        for (Point2D p : points) {
+            int column = (int) p.getX();
+            int row = (int) p.getY();
+
+            Disc disc = getDisc(column, row).orElse(new Disc(!firstPlayerMove));
+            if (disc.firstPlayer == firstPlayerMove) {
+                sequence++;
+                if (sequence >= 4) {
+                    return true;
+                }
+            } else {
+                sequence = 0;
+            }
+        }
+        return false;
+    }
+
+    private void gameOver() {
+        System.out.println("Game is Finished");
+        System.out.println((firstPlayerMove ? " Green" : "Yellow") + " Player Won");
+
+    }
+    private Optional<Disc> getDisc(int column, int row) {
+        if ((column < 0 || column >= COLUMNS)
+                || (row < 0 || row >= ROWS))
+            return Optional.empty();
+        return Optional.ofNullable(grid[column][row]);
+    }
+
+    public boolean isFirstPlayerMove() {
+        return firstPlayerMove;
+    }
 }
